63d843dd148ea894fd9b17baccdbedcab80492d2
Kavita Poduri <kavi.pdri@gmail.com>
2021-11-29 11:25:42 -0800

    implemented groceryListItem as class component

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+// var GroceryList = () => (
+//   <div>
+//     <ul>
+//       {
+//         groceryArray.map((listItem) => (
+//           <li >{listItem}</li>
+//         ))
+//       }
+//     </ul>
+//   </div>
+// );
+
+var listArray = ['Chicken & maple sausages', 'Frozen pizza - BBQ chicken', 'Pepper turkey'];
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <li>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryArray.map(groceryArrayElement =>
+      <GroceryListItem groceryItem = {groceryArrayElement} />
+    )}
+  </ul>
+)
+
+ReactDOM.render(<GroceryList groceryArray = {listArray} />, document.getElementById("app"));
